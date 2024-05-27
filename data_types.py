a65453d772d58b14e634f30b35e4743cf40f1905
Elistonex <elliotieno18@gmail.com>
2024-05-27 19:57:48 +0300

    init

---
+++ data_types.py
@@ -0,0 +1,37 @@
+Num1 = 11
+print((Num1), 'Is of type' ,type(Num1))
+
+Num2 = 7.4
+print((Num2), 'Is of type' ,type(Num2))
+
+Num3 = 1+2j
+print((Num3), 'Is of type' ,type(Num3))
+
+product = ('Microsoft', 'Xbox', '49.99')
+print(product[2])
+#Create a set named student ID
+Student_id = {110,24,12,245,212,300}
+
+#Display student Id elements
+print(Student_id)
+
+#Display the datatype of Student Id
+print(type(Student_id))
+
+#create a dictionary named capital city
+Capital_city = {'Kenya':'Nairobi', 'Italy':'Rome','Egypt':"cairo"}
+
+ #Display the capital cities
+print(Capital_city)
+
+ #Print the capital city of Italy
+print(Capital_city['Italy'])
+
+Integer_number = 144
+Float_number =1.35
+New_number = (Integer_number+Float_number)
+
+#Display the new number and resulting data type
+print(New_number)
+print("Data type:",type(New_number))
+
---
+++ user_input.py
@@ -0,0 +1,11 @@
+# This input function asks the user for their name and stores it in a variable called name.
+name = input('Name: ')
+
+# This input function asks the user for their age and stores it in a variable called age.
+age = input('Age: ')
+
+# This input function asks the user for their location and stores it in a variable called location.
+location = input('Location: ')
+
+# This prints a customized message using all the variables.
+print(f'Hello {name}, you are {age} years old and live in {location}.')
